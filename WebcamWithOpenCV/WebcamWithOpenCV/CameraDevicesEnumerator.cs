f1b28eb7ee8fb75d5cae297c70cbdcbe2c93dcc1
Francesco Bonizzi <fbonizzi.90@gmail.com>
2020-06-09 08:57:02 +0200

    Removed PNPClass = 'Image' from CameraDevicesEnumerator because it was selecting some printer

--- WebcamWithOpenCV/WebcamWithOpenCV/CameraDevicesEnumerator.cs
+++ WebcamWithOpenCV/WebcamWithOpenCV/CameraDevicesEnumerator.cs
@@ -8,7 +8,7 @@ namespace WebcamWithOpenCV
         public static List<string> GetAllConnectedCameras()
         {
             var cameraNames = new List<string>();
-            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE (PNPClass = 'Image' OR PNPClass = 'Camera')"))
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE (PNPClass = 'Camera')"))
             {
                 foreach (var device in searcher.Get())
                 {
