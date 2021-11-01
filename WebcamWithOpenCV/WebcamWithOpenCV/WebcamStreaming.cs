5bb6eed2e17a65b45fb4a7699d2660c3b96855d2
Francesco Bonizzi <fbonizzi.90@gmail.com>
2021-11-01 08:52:14 +0100

    Removed some useless comments

--- WebcamWithOpenCV/WebcamWithOpenCV/WebcamStreaming.cs
+++ WebcamWithOpenCV/WebcamWithOpenCV/WebcamStreaming.cs
@@ -76,7 +76,7 @@ namespace WebcamWithOpenCV
                                 {
                                     try
                                     {
-                                        string qrCodeData = detectBarcode(frame, 120);
+                                        string qrCodeData = DetectBarcode(frame, 120);
                                         OnQRCodeRead.Invoke(
                                             this,
                                             new QRCodeReadEventArgs(qrCodeData));
@@ -126,30 +126,22 @@ namespace WebcamWithOpenCV
             }
         }
 
-        private static void rotateImage(Mat src, Mat dst, double angle, double scale)
+        private static void RotateImage(Mat src, Mat dst, double angle, double scale)
         {
             var imageCenter = new Point2f(src.Cols / 2f, src.Rows / 2f);
             var rotationMat = Cv2.GetRotationMatrix2D(imageCenter, angle, scale);
             Cv2.WarpAffine(src, dst, rotationMat, src.Size());
         }
 
-
-        private static string detectBarcode(Mat mat, double thresh, bool debug = false, double rotation = 0)
+        private static string DetectBarcode(Mat mat, double thresh, bool debug = false, double rotation = 0)
         {
-
             // load the image and convert it to grayscale
             var image = mat;
 
             if (rotation != 0)
             {
-                rotateImage(image, image, rotation, 1);
+                RotateImage(image, image, rotation, 1);
             }
-
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Source", image);
-            //    Cv2.WaitKey(1); // do events
-            //}
 
             var gray = new Mat();
             var channels = image.Channels();
@@ -162,28 +154,18 @@ namespace WebcamWithOpenCV
                 image.CopyTo(gray);
             }
 
-
             // compute the Scharr gradient magnitude representation of the images
             // in both the x and y direction
             var gradX = new Mat();
             Cv2.Sobel(gray, gradX, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
-            //Cv2.Scharr(gray, gradX, MatType.CV_32F, xorder: 1, yorder: 0);
 
             var gradY = new Mat();
             Cv2.Sobel(gray, gradY, MatType.CV_32F, xorder: 0, yorder: 1, ksize: -1);
-            //Cv2.Scharr(gray, gradY, MatType.CV_32F, xorder: 0, yorder: 1);
-
+  
             // subtract the y-gradient from the x-gradient
             var gradient = new Mat();
             Cv2.Subtract(gradX, gradY, gradient);
             Cv2.ConvertScaleAbs(gradient, gradient);
-
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Gradient", gradient);
-            //    Cv2.WaitKey(1); // do events
-            //}
-
 
             // blur and threshold the image
             var blurred = new Mat();
@@ -192,39 +174,17 @@ namespace WebcamWithOpenCV
             var threshImage = new Mat();
             Cv2.Threshold(blurred, threshImage, thresh, 255, ThresholdTypes.Binary);
 
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Thresh", threshImage);
-            //    Cv2.WaitKey(1); // do events
-            //}
-
-
             // construct a closing kernel and apply it to the thresholded image
             var kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(21, 7));
             var closed = new Mat();
             Cv2.MorphologyEx(threshImage, closed, MorphTypes.Close, kernel);
 
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Closed", closed);
-            //    Cv2.WaitKey(1); // do events
-            //}
-
-
             // perform a series of erosions and dilations
             Cv2.Erode(closed, closed, null, iterations: 4);
             Cv2.Dilate(closed, closed, null, iterations: 4);
 
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Erode & Dilate", closed);
-            //    Cv2.WaitKey(1); // do events
-            //}
-
-
             //find the contours in the thresholded image, then sort the contours
             //by their area, keeping only the largest one
-
             Point[][] contours;
             HierarchyIndex[] hierarchyIndexes;
             Cv2.FindContours(
@@ -242,7 +202,7 @@ namespace WebcamWithOpenCV
             var contourIndex = 0;
             var previousArea = 0;
             var biggestContourRect = Cv2.BoundingRect(contours[0]);
-            while ((contourIndex >= 0))
+            while (contourIndex >= 0)
             {
                 var contour = contours[contourIndex];
 
@@ -257,32 +217,18 @@ namespace WebcamWithOpenCV
                 contourIndex = hierarchyIndexes[contourIndex].Next;
             }
 
-
-            /*biggestContourRect.Width += 10;
-            biggestContourRect.Height += 10;
-            biggestContourRect.Left -= 5;
-            biggestContourRect.Top -= 5;*/
-
-
             var barcode = new Mat(image, biggestContourRect); //Crop the image
             Cv2.CvtColor(barcode, barcode, ColorConversionCodes.BGRA2GRAY);
 
-           // Cv2.ImShow("Barcode", barcode);
-        //    Cv2.WaitKey(1); // do events
-
             var barcodeClone = barcode.Clone();
-            var barcodeText = getBarcodeText(barcodeClone);
+            var barcodeText = GetBarcodeText(barcodeClone);
 
             if (string.IsNullOrWhiteSpace(barcodeText))
             {
-                Console.WriteLine("Enhancing the barcode...");
-                //Cv2.AdaptiveThreshold(barcode, barcode, 255,
-                //AdaptiveThresholdType.GaussianC, ThresholdType.Binary, 9, 1);
-                //var th = 119;
                 var th = 100;
                 Cv2.Threshold(barcode, barcode, th, 255, ThresholdTypes.Tozero);
                 Cv2.Threshold(barcode, barcode, th, 255, ThresholdTypes.Binary);
-                barcodeText = getBarcodeText(barcode);
+                barcodeText = GetBarcodeText(barcode);
             }
 
             Cv2.Rectangle(image,
@@ -291,29 +237,17 @@ namespace WebcamWithOpenCV
                 new Scalar(0, 255, 0),
                 2);
 
-            //if (debug)
-            //{
-            //    Cv2.ImShow("Segmented Source", image);
-            //    Cv2.WaitKey(1); // do events
-            //}
-
-        //    Cv2.WaitKey(0);
-        //    Cv2.DestroyAllWindows();
-
             return barcodeText;
         }
 
 
-        private static string getBarcodeText(Mat barcode)
+        private static string GetBarcodeText(Mat barcode)
         {
             // `ZXing.Net` needs a white space around the barcode
             var barcodeWithWhiteSpace = new Mat(new Size(barcode.Width + 30, barcode.Height + 30), MatType.CV_8U, Scalar.White);
             var drawingRect = new Rect(new Point(15, 15), new Size(barcode.Width, barcode.Height));
             var roi = barcodeWithWhiteSpace[drawingRect];
             barcode.CopyTo(roi);
-
-      //      Cv2.ImShow("Enhanced Barcode", barcodeWithWhiteSpace);
-            Cv2.WaitKey(1); // do events
 
             return decodeBarcodeText(barcodeWithWhiteSpace.ToBitmap());
         }
@@ -322,28 +256,15 @@ namespace WebcamWithOpenCV
         {
             var source = new BitmapLuminanceSource(barcodeBitmap);
 
-            // using http://zxingnet.codeplex.com/
-            // PM> Install-Package ZXing.Net
             var reader = new BarcodeReader(null, null, ls => new GlobalHistogramBinarizer(ls))
             {
                 AutoRotate = true,
                 TryInverted = true,
                 Options = new DecodingOptions
                 {
-                    TryHarder = true,
-                    //PureBarcode = true,
-                    /*PossibleFormats = new List<BarcodeFormat>
-                    {
-                        BarcodeFormat.CODE_128
-                        //BarcodeFormat.EAN_8,
-                        //BarcodeFormat.CODE_39,
-                        //BarcodeFormat.UPC_A
-                    }*/
+                    TryHarder = true
                 }
             };
-
-            //var newhint = new KeyValuePair<DecodeHintType, object>(DecodeHintType.ALLOWED_EAN_EXTENSIONS, new Object());
-            //reader.Options.Hints.Add(newhint);
 
             var result = reader.Decode(source);
             if (result == null)
